62b9acc0c7bb414bdaca2f7f9223e12f263d6b0a
Nikita Smirnov <notlol159@gmail.com>
2025-01-15 12:57:37 +0300

    update_music

--- select.sql
+++ select.sql
@@ -4,7 +4,7 @@ select title,duration from song
 where duration = (select max(duration) from song);
 --2
 select title from song
-where duration > 210;
+where duration >= 210;
 --3
 select title from collection
 where year >= 2018 and year <= 2020;
@@ -13,7 +13,14 @@ select name from performer
 where name not like '% %';
 --5
 select title from song 
-where title like '%мой%' or title like '%my%';
+where title ILIKE 'my %'
+or title ILIKE '% my'
+OR title ILIKE '% my %'
+or title ilike 'my' 
+or title ILIKE 'мой %'
+or title ILIKE '% мой'
+OR title ILIKE '% мой %'
+or title ilike 'мой';
 
 --- Задание 3
 --1
@@ -31,18 +38,22 @@ select album.title,avg(song.duration) from album
 join song on album.id = song.album_id
 group by album.title;
 --4
-select performer.name,album.year from performeralbum p 
-join performer on p.performer_id = performer.id 
-join album on p.album_id = album.id 
-where year != 2020 ;
+SELECT name FROM performer
+WHERE name NOT IN (SELECT name FROM performer 
+    JOIN performeralbum pa ON performer.id = pa.performer_id
+    JOIN album ON album.id = pa.album_id 
+    WHERE album.year = 2020
+   );
 --5
-select collection.title, performer.name from collectionsong c 
+select distinct collection.title, performer.name from collectionsong c 
 join collection on c.collection_id = collection.id 
 join song on c.song_id = song.id 
 join album on song.album_id = album.id 
 join performeralbum p on album.id = p.album_id 
 join performer on p.performer_id = performer.id
-where performer.name like '%Исп2%';
+where performer.name like 'Исп2';
+
+select * from song;
 
 
 
